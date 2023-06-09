3fb7cb2a181ccb9985a7d4efdece19e00601a18f
ubilby <ubilby@gmail.com>
2023-06-09 12:45:06 +0400

    5 тестов

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -97,7 +97,17 @@ class IsAdminOrReadOnly(permissions.BasePermission):
 
 class IsAdmin(permissions.BasePermission):
     def has_permission(self, request, view):
-        return request.user.is_admin or request.user.is_superuser
+        return (
+            request.user.is_authenticated
+            and (
+                request.user.is_admin or request.user.is_superuser
+            )
+        )
 
     def has_object_permission(self, request, view, obj):
-        return request.user.is_admin or request.user.is_superuser
+        return (
+            request.user.is_authenticated
+            and (
+                request.user.is_admin or request.user.is_superuser
+            )
+        )
